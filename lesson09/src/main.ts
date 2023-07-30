11c19d7c8d89f79a86663d32f5170e5304c2d127
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-07-30 22:03:57 +0200

    interface with objects

--- lesson09/src/main.ts
+++ lesson09/src/main.ts
@@ -101,4 +101,52 @@ type NamesOnly = NonNullable<AllPossibleGrades>
 
  // paramaeters
 
- 
+ interface Home {
+    readonly resident: { name: string; age: number };
+  }
+   
+  function visitForBirthday(home: Home) {
+    // We can read and update properties from 'home.resident'.
+    console.log(`Happy birthday ${home.resident.name}!`);
+    home.resident.age++;
+  }
+   
+  function evict(home: Home) {
+    // But we can't write to the 'resident' property itself on a 'Home'.
+    home.resident = {
+  Cannot assign to 'resident' because it is a read-only property.
+      name: "Victor the Evictor",
+      age: 42,
+    };
+  }
+
+  interface Person {
+    name: string;
+    age: number;
+  }
+   
+  interface ReadonlyPerson {
+    readonly name: string;
+    readonly age: number;
+  }
+   
+  let writablePerson: Person = {
+    name: "Person McPersonface",
+    age: 42,
+  };
+   
+  // works
+  let readonlyPerson: ReadonlyPerson = writablePerson;
+   
+  console.log(readonlyPerson.age); // prints '42'
+  writablePerson.age++;
+  console.log(readonlyPerson.age); // prints '43'
+
+  interface StringArray {
+    [index: number]: string;
+  }
+   
+  const myArray: StringArray = getStringArray();
+  const secondItem = myArray[1];
+            
+  
