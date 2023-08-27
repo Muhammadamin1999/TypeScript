fab8d9e83b7b78d5679a4848412abdf1504ae4ab
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-08-27 11:47:48 +0200

    ts review

--- lesson05/src/main.ts
+++ lesson05/src/main.ts
@@ -3,32 +3,32 @@ type Two = string | number
 type Three = 'hello'
 
 // convert to more or less specific
-
-let a: One = 'hello';
-let b = a as Two;     // example for less specific type
-let c = a as Three
+let a: One = 'hello'
+let b = a as Two // less specific type
+let c = a as Three // more specific
 
 let d = <One>'world'
-let e = <string>'world'
+let e = <string | number>'world'
+const addOrConcat = (a: number,b: number, c: 'add' | 'concat'): number | string =>{
+    if(c === 'add'){
+        return a + b;
+    }else{
+        return '' + a + b;
+    }
 
-const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | 
-string =>{
-    if(c === 'add') return a + b
-    return '' + a + b
 }
 
 let myVal: string = addOrConcat(2,2,'concat') as string
-// be carefull because TS sees no problem but string is returned
-let nextVal: number = addOrConcat(2,2,'concat') as number
-// unknown
+
+// be careful ts sees no problem but a string is returned
+let nextval: number = addOrConcat(2,2,'concat') as number
 
 (10 as unknown) as string
 
-// te DOM
-const img = document.querySelector('img')!
-const myImg = document.getElementById('#img') as HTMLImageElement
-const nextImg = <HTMLImageElement>document.getElementById('#img') 
-img.src 
-myImg.src
+// the DOM
 
+const img = document.querySelector('img')!
+const myImg = document.getElementById('#img')as HTMLImageElement
+
+myImg.src
 
