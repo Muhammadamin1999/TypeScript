005a11e1579aaf25c43599ab4772062862f2c1cb
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-08-31 22:05:36 +0200

    ts review

--- everydat_types/src/w3.ts
+++ everydat_types/src/w3.ts
@@ -83,8 +83,132 @@ const nameAgeMap: {[index: string]: number} = {}
 nameAgeMap.Jack = 25;
 //nameAgeMap.Mark = "Fiftty"// gives an error string not assignable
 
+// enum is a special class that represents a group of constants(unchangable variables)
 
+// enums come in two flavors string and numeric
 
+enum Statusodes{
+    NotFound = 404,
+    Success = 200,
+    Accepted = 2002,
+    Badrequest = 400
+}
+
+console.log(Statusodes.NotFound);
+
+// string enums
+
+enum Directions{
+    North = "North",
+    South = "South",
+    East = "East",
+    West = "West"
+}
+
+// types Allies allow defining types with a custom name used for string, object and arrays
+
+type CarYear = number
+type CarType = string
+type CarModel = string
+type Car = {
+    year: CarYear,
+    type: CarType,
+    model: CarModel
+}
+
+const carYear: CarYear = 2001
+const carType: CarType = "Toyota"
+const carModel: CarModel = "Corolla"
+const card: Car = {
+    year: carYear,
+    type: carType,
+    model: carModel
+}
+// interfaces like types but used for obj
+// extending interfaces
+interface Rectangle{
+    height: number,
+    width: number
+}
+interface ColoredRectangle extends Rectangle{
+    color: string
+}
+const rectangle: Rectangle = {
+    height: 20, 
+    width: 10
+    
+}
+const coloredRectangle: ColoredRectangle={
+    height: 20, 
+    width: 10,
+    color: "red"
+}
+
+// type aliases help to give custom names to types
+ type Points ={
+    x: number,
+    y: number
+ }
+
+ const points: Point = {x: 10, y: 20};
+
+ // as function signatures
+
+ type Mathoperation = (a: number, b: number)=> number;
+
+ const adding: Mathoperation = (a,b)=> a+b;
+
+// optional params
+
+function addition(a:number, b:number,c?:number){
+    return a + b + (c||0);
+}
+
+// default params
+
+function pow(value: number, exponent: number = 10){
+    return value ** exponent;
+}
+ 
+// named params
+
+function divide({dividend, divisor}:{dividend: number, divisor: number}){
+    return dividend / divisor;
+}
+
+// rest params
+
+function examples(a: number, b: number, ...rest: number[]){
+    return a + b + rest.reduce((p,c)=> p + c,0);
+}
+
+type Negate = (value: number) => number;
+
+const negateFunction: Negate = (value)=> value * 1;
+// casting is a ovveriding a type
+// first option to cast with as
+
+let h: unknown = "hello";
+console.log((x as string).length); // it shows an error; as x is string here
+
+// casting with <>
+
+let g: unknown = "hello";
+
+console.log((<string>x).length);
+
+// force casting
+
+let j = "hello";
+//console.log(((j as unknown)as number).length)
+ 
+// class
+
+// class Persons{
+//     namely: string
+// }
+// const person3 = new Persons()
+// person3.namely = "Joe"
 
 
 
